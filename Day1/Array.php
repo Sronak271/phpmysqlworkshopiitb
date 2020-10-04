668aac0633f00284c389f1583a6410900e406d37
Ronak Singh <singhrora19ce@student.mes.ac.in>
2020-10-04 19:52:01 +0530

    second commit

--- Day1/Array.php
+++ Day1/Array.php
@@ -1,6 +1,6 @@
 <?php
 
-$week = array(1=>'Sunday', 2=>'Monday', 3=>'Tuesday', 4=> 'Wednesday', 5=>'Thursday', 6=>'Friday', 7=>'Saturday' );
+$week = array(1=>'Sunday', 2=>'Monday', 3=>'Tuesday', 4=>'Wednesday', 5=>'Thursday', 6=>'Friday', 7=>'Satuday');
 $i;
 
       for($i = 1; $i < 8; $i++ )
