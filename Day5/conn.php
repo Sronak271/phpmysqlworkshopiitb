f66895caadf97d47dec7d9e405c32cfaa6d3f071
Ronak Singh <singhrora19ce@student.mes.ac.in>
2020-10-11 22:32:49 +0530

    fifth commit

---
+++ Day5/conn.php
@@ -0,0 +1,7 @@
+<?php
+
+$conn =mysqli_connect("localhost","Ronak","Ronak@2001","phpmysqlworkshop") or die("Connection failed");
+
+echo "<b>Please fill the Data!</b> <br>";
+
+?>
---
+++ Day5/q1_md5.php
@@ -0,0 +1,35 @@
+<?php
+
+require("conn.php");
+
+@$username = $_POST['Username'];
+@$userpassword = $_POST['Password'];
+
+
+
+$userpasswordenc = md5($userpassword);
+
+if($username && $userpasswordenc)
+{
+             $write = "INSERT INTO data1 VALUES('$username','$userpasswordenc')";
+             
+             if($conn-> query ($write) == TRUE)
+                        {
+                         echo"<br><u><i>Record created successfully!</i><>/u";
+                         }
+             else
+                 {
+                 echo"Error: ".$write. "<br>" .$connect1->error;
+                 }
+}
+$conn->close();
+
+
+?>
+
+<form action ='q1_md5.php' method = 'POST'>
+Enter Username:  <input type = 'name' name = 'Username'><br><br>
+Enter Your Password: <input type = 'text' name = 'Password'>
+                     <input type = 'Sumbit' name = 'Login'>
+
+</form>
